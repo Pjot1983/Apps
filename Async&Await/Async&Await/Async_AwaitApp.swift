160d846c2e87c2c0bafa852aec3346560ef2ca7c
Pjot1983 <105443913+Pjot1983@users.noreply.github.com>
2022-11-28 17:16:30 +0100

    Add files via upload

---
+++ Async&Await/Async&Await/Async_AwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_AwaitApp.swift
+//  Async&Await
+//
+//  Created by Pjot on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Async_AwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
